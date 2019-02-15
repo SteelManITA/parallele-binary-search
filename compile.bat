15344ef2bb5e36291b223d5d901d29fcc7e51fb3
SteelMan <concetto.pollicina@gmail.com>
2019-02-15 23:10:37 +0100

    search 2key

--- compile.bat
+++ compile.bat
@@ -2,11 +2,12 @@
 setlocal enabledelayedexpansion
 
 SET FILE_NAME=natural-merge
+SET /A rand=%RANDOM%
 
-set "commands[0]=rm -rf compile\* dist\* vc140.pdb"
-set "commands[1]=nvcc -g -O3 .\src\%FILE_NAME%.cu -o compile\%FILE_NAME%"
-set "commands[2]=copy compile\%FILE_NAME%.exe dist\%FILE_NAME%.exe"
-set "commands[3]=.\dist\%FILE_NAME%.exe %1"
+set "commands[0]=rm -rf compile\* vc140.pdb"
+set "commands[1]=nvcc -g -O3 -arch=sm_35 -rdc=true -lineinfo .\src\%FILE_NAME%.cu -o compile\%FILE_NAME%-%rand%"
+set "commands[2]=copy compile\%FILE_NAME%-%rand%.exe dist\%FILE_NAME%-%rand%.exe"
+set "commands[3]=.\dist\%FILE_NAME%-%rand%.exe %1"
 
 set "command=%commands[0]%"
 set "i=1"
--- src/device.cu
+++ src/device.cu
@@ -1,14 +1,13 @@
 __device__ __forceinline__
 int search(
     const int * __restrict__ vec,
-    const int search,
-    const int length
+    int lower,
+    int upper,
+    const int search
 ) {
-    int lower = 0;
-    int upper = length;
     int middle;
     while (lower < upper) {
-        middle = (lower + upper) >> 1;
+        middle = lower + ((upper - lower) >> 1);
         if (vec[middle] < search) {
             lower = middle + 1;
         } else {
@@ -16,6 +15,48 @@ int search(
         }
     }
     return lower;
+}
+
+__device__ __forceinline__
+int2 search_2key(
+    const int * __restrict__ arr,
+    int left,
+    int right,
+    const int small_key,
+    const int large_key
+) {
+    int middle;
+    int2 out = make_int2(-1, -1);
+    while (left <= right) {
+        middle = left + ((right - left) >> 1);
+        if (arr[middle] < small_key)  {
+            left = middle+1;
+        } else if (arr[middle] == small_key) {
+            out.x = middle;
+            out.y = search(arr, middle+1, right, large_key);
+            break;
+        } else if (arr[middle] > small_key && arr[middle] < large_key) {
+            if (left <= middle) {
+                out.x = search(arr, left, middle, small_key);
+            } else {
+                out.x = middle;
+            }
+
+            if (middle+1 <= right) {
+                out.y = search(arr, middle+1, right+1, large_key);
+            } else {
+                out.y = middle+1;
+            }
+            break;
+        } else if (arr[middle] == large_key) {
+            out.y = middle;
+            out.x = search(arr, left, middle-1, small_key);
+            break;
+        } else if (arr[middle] > large_key) {
+            right = middle-1;
+        }
+    }
+    return out;
 }
 
 __global__
@@ -33,8 +74,8 @@ void init(
 
 __global__
 void merge(
-    const int * __restrict__ v1,
-    const int * __restrict__ v2,
+    const int2 * __restrict__ v1,
+    const int2 * __restrict__ v2,
     int * __restrict__ vmerge,
     int numels
 ) {
@@ -51,16 +92,17 @@ void merge(
     }
     */
     int i = getId();
-    if (i >= numels) return;
+    if (i >= numels/2) return;
 
-    int el1 = v1[i];
-    int el2 = v2[i];
+    int2 el1 = v1[i];
+    int2 index_el1_in_v2 = search_2key((int*)v2, 0, numels-1, el1.x, el1.y); // 2*i;
+    vmerge[(i*2) + (index_el1_in_v2.x + 1)] = el1.x;
+    vmerge[(i*2) + (index_el1_in_v2.y + 1)] = el1.y;
 
-    int index_el1_in_v2 = search(v2, el1, numels); // 2*i;
-    int index_el2_in_v1 = search(v1, el2, numels); // i1+1;
-
-    vmerge[i+index_el1_in_v2] = el1;
-    vmerge[i+index_el2_in_v1] = el2;
+    int2 el2 = v2[i];
+    int2 index_el2_in_v1 = search_2key((int*)v1, 0, numels-1, el2.x, el2.y); // i1+1;
+    vmerge[(i*2) + (index_el2_in_v1.x + 1)] = el2.x;
+    vmerge[(i*2) + (index_el2_in_v1.y + 1)] = el2.y;
 
     // sincronizza e aggiungi i mancanti
     // __syncthreads();
